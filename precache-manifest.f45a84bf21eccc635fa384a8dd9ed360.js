dd131cd2162655f77b5b131ff31477cf1ab47f41
you-jin-lee <youjinlee1997@gmail.com>
2020-10-10 20:52:57 +0900

    Updates

---
+++ precache-manifest.f45a84bf21eccc635fa384a8dd9ed360.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f678a10b52f151501d67190db606e92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c399615ef1ccf85d28d7",
+    "url": "/static/css/main.3b0dd50d.chunk.css"
+  },
+  {
+    "revision": "11748d6dc8da0fe94e79",
+    "url": "/static/js/2.8f3b569d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f3b569d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c399615ef1ccf85d28d7",
+    "url": "/static/js/main.e604904d.chunk.js"
+  },
+  {
+    "revision": "bd7f4f55b55e38cea2f1",
+    "url": "/static/js/runtime-main.58002af7.js"
+  },
+  {
+    "revision": "291f648f0276e5d10cbe13efdd5eca4e",
+    "url": "/static/media/orange.291f648f.jpg"
+  }
+]);
